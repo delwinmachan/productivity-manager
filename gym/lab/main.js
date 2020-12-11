8b61cefdfee86456721a8d062bbf2728681998ee
delwinmachan <68820194+delwinmachan@users.noreply.github.com>
2020-12-11 10:30:02 +0530

    pekas

--- gym/lab/main.js
+++ gym/lab/main.js
@@ -1,10 +1,10 @@
 var pageCounter = 1;
-var bookContainer = document.getElementById("book-info");
+var bookContainer = document.getElementById("work-info");
 var btn = document.getElementById("btn");
 
 btn.addEventListener("click", function() {
   var ourRequest = new XMLHttpRequest();
-  ourRequest.open('GET', 'book-' + pageCounter + '.json');
+  ourRequest.open('GET', 'work-' + pageCounter + '.json');
   ourRequest.onload = function() {
     if (ourRequest.status >= 200 && ourRequest.status < 400) {
       var ourData = JSON.parse(ourRequest.responseText);
@@ -30,23 +30,23 @@ function renderHTML(data) {
   var htmlString = "";
 
   for (i = 0; i < data.length; i++) {
-    htmlString += "<p>" + data[i].name + " --trained by-- " + data[i].author + " ";
+    htmlString += "<p>" + data[i].name + " --trained by-- " + data[i].trainer + " ";
     
-    for (ii = 0; ii < data[i].detail.release.length; ii++) {
+    for (ii = 0; ii < data[i].detail.start.length; ii++) {
       if (ii == 0) {
-        htmlString += data[i].detail.release[ii];
+        htmlString += data[i].detail.start[ii];
       } else {
-        htmlString += " --starting on-- " + data[i].detail.release[ii];
+        htmlString += " --starting on-- " + data[i].detail.start[ii];
       }
     }
 
     htmlString += ' ---prize.--- ';
 
-    for (ii = 0; ii < data[i].detail.type.length; ii++) {
+    for (ii = 0; ii < data[i].detail.prize.length; ii++) {
       if (ii == 0) {
-        htmlString += data[i].detail.type[ii];
+        htmlString += data[i].detail.prize[ii];
       } else {
-        htmlString += "  " + data[i].detail.type[ii];
+        htmlString += "  " + data[i].detail.prize[ii];
       }
     }
 
